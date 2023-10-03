82503f708fb56cbe50e7439124a9269ce6552e5e
tiennguyen691620 <tiennguyen691620@gmail.com>
2023-10-03 10:08:08 +0700

    Day 14

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -7,6 +7,7 @@ import productApi from 'src/apis/product.api'
 import Pagination from 'src/Components/Pagination'
 import { ProductListConfig } from 'src/types/product.type'
 import { isUndefined, omitBy } from 'lodash'
+import categoryApi from 'src/apis/category.api'
 
 export type QueryConfig = {
   [key in keyof ProductListConfig]: string
@@ -24,31 +25,41 @@ export default function ProductList() {
       order: queryParams.order,
       price_max: queryParams.price_max,
       price_min: queryParams.price_min,
-      rating_filter: queryParams.rating_filter
+      rating_filter: queryParams.rating_filter,
+      category: queryParams.category,
     },
     isUndefined
   )
-  const { data } = useQuery({
+  const { data: productData } = useQuery({
     queryKey: ['products', queryConfig],
     queryFn: () => {
       return productApi.getProducts(queryConfig as ProductListConfig)
     },
     keepPreviousData: true
   })
-  console.log(data)
+  const { data: categoriesData } = useQuery({
+    queryKey: ['categories', queryConfig],
+    queryFn: () => {
+      return categoryApi.getCategories()
+    },
+    keepPreviousData: true
+  })
 
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
-        {data && (
+        {productData && (
           <div className='grid grid-cols-12 gap-6'>
             <div className='col-span-3'>
-              <AsideFilter></AsideFilter>
+               <AsideFilter queryConfig={queryConfig} categories={categoriesData?.data.data || []}></AsideFilter>
             </div>
             <div className='col-span-9'>
-              <SortProductList queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size}></SortProductList>
+              <SortProductList
+                queryConfig={queryConfig}
+                pageSize={productData.data.data.pagination.page_size}
+              ></SortProductList>
               <div className='mt-6 grid grid-cols-2 md:gird-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-                {data.data.data.products.map((product) => (
+                {productData.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
                     <Product product={product}></Product>
                   </div>
@@ -56,7 +67,7 @@ export default function ProductList() {
               </div>
               <Pagination
                 queryConfig={queryConfig}
-                pageSize={data.data.data.pagination.page_size}
+                pageSize={productData.data.data.pagination.page_size}
               ></Pagination>
             </div>
           </div>
